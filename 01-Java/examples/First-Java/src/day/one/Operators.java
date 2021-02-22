0363470740a604c6f6dc7b03acb9e0462650b3b1
connellrobert <robert.lel.connell@gmail.com>
2021-02-22 16:59:57 -0600

    adding end of day 1

---
+++ 01-Java/examples/First-Java/src/day/one/Operators.java
@@ -0,0 +1,43 @@
+//packages work with access modifiers to help encapsulate the information.
+// //   It is useful for organizing our classes
+package day.one;
+
+public class Operators {
+
+    /*
+    numerical operations
+        +   addition
+        -   subtraction
+        /   division
+        *   multiplication
+        %   modulus
+        =   assignments
+        +=  addition assignment
+        -=  subtraction assignment
+        /=  division assignment
+        *=  multiplication assignment
+        %=  modulus assignment
+
+    logical operations
+        <   less than
+        >   Greater than
+        <=  less than or equal
+        >=  greater than or equal to
+        ==  equals
+        !   not (works on booleans)
+        !=  not equal to
+        |   logical or
+        &   logical and
+        ^   xor
+        &&  and (short circuit)
+        ||  or (short circuit)
+
+     ternary
+        ? : String s = i < 6 ? "yes" : "no"
+            like an if else statement that returns something
+     */
+
+
+
+
+}
