136e33018a1b2326832b21180980f1ca2b1f114b
connellrobert <robert.lel.connell@gmail.com>
2021-03-15 10:59:46 -0500

    adding beginning of multithreading

---
+++ 04-threads/src/main/java/dev/enterprise/problems/Deadlock.java
@@ -0,0 +1,68 @@
+package dev.enterprise.problems;
+
+import java.time.LocalTime;
+
+public class Deadlock {
+
+    Object keyA = new Object();
+    Object keyB = new Object();
+
+
+    public void a() {
+        synchronized (keyA) {
+            System.out.println(Thread.currentThread().getName() + ": " + LocalTime.now());
+            b();
+        }
+    }
+
+    public void b() {
+        synchronized (keyB) {
+            System.out.println(Thread.currentThread().getName() + ": " +  LocalTime.now());
+            c();
+        }
+    }
+
+    public void c() {
+        synchronized (keyA){
+            System.out.println(Thread.currentThread().getName() + ": " + LocalTime.now());
+            System.out.println("in c");
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Deadlock d = new Deadlock();
+
+        Runnable r1 = () -> d.a();
+        Runnable r2 = () -> d.b();
+
+        Thread t2 = new Thread(r2);
+        Thread t1 = new Thread(r1);
+
+        t2.start();
+        t1.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("main completed");
+
+
+//        ReadAndWrite r = new ReadAndWrite();
+//        Thread t3 = new Thread(() -> {
+//            for (int i = 0; i < 500; i++) System.out.println(r.getX());
+//        });
+//        Thread t4 = new Thread(() -> {
+//            for (int i = 0; i < 500; i++) r.setX(i);
+//        });
+//
+//        t4.start();
+//        t3.start();
+//
+//        t4.join();
+//        t3.join();
+//
+//        System.out.println("main waiting for it");
+
+    }
+}
---
+++ 04-threads/src/main/java/dev/enterprise/problems/RaceCondition.java
@@ -0,0 +1,45 @@
+package dev.enterprise.problems;
+
+import dev.enterprise.threads.CustomThread;
+
+public class RaceCondition {
+
+    int x = 5;
+
+
+    //The synchronized keyword makes a method only allow
+    //      one thread at a time into the execution.
+    //      This solves race conditions, but causes some others.
+    public synchronized void incr(){
+        x = x+1;
+    }
+
+    public synchronized int getX(){
+        return x;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        RaceCondition r = new RaceCondition();
+
+//        System.out.println(Thread.currentThread().getName());
+//        r.incr();
+//        r.incr();
+
+        CustomThread T1 = new CustomThread(r);
+        CustomThread T2 = new CustomThread(r);
+
+        //Why not call run here?
+        //      run will not call a new thread, but instead run on the main thread
+        T1.start();
+        T2.start();
+
+        //Wait for threads to complete and then continue on the current thread
+        T2.join();
+        T1.join();
+
+
+
+        System.out.println(r.getX());
+
+    }
+}
